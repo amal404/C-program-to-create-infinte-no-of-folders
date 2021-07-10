441a7c7f652ce7e966878924ab76b320543d691b
AMAL SEBASTIAN <64699226+amal404@users.noreply.github.com>
2021-07-11 00:12:54 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void main()
+{
+	int i=1,n;
+	char p[10],k[54];
+	strcpy(k, "mkdir ");
+	printf("limit :");
+	scanf("%d",&n);
+	while(i<=n){		
+			sprintf(p, "%d",i);
+			strcat(k,p);
+			system(k);
+			k[strlen(k) - strlen(p)]  = '\0';
+			i++;
+			printf("\ncreated dir: %d",i);
+	}
+}
+
+
